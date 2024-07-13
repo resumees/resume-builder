b3c46d4ea2517e4e395d1fa31c966e195c2c0c07
F-Joubert <95570212+F-Joubert@users.noreply.github.com>
2024-07-13 14:17:17 +1000

    Create campaign modal
    Added modal component to campaign.tsx

--- frontend/src/components/Campaign.tsx
+++ frontend/src/components/Campaign.tsx
@@ -1,9 +1,50 @@
-import { Box, Button } from "@chakra-ui/react";
+/* eslint-disable react/react-in-jsx-scope */
+import { 
+  Box, 
+  Button, 
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  Textarea,
+  FormControl,
+  FormLabel,
+  useDisclosure
+} from "@chakra-ui/react";
 
 const Campaign: React.FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Box>
-      <Button colorScheme="blue">Click Me</Button>
+      <Button colorScheme="blue" onClick={onOpen}>Create Campaign</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create New Campaign</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Campaign Title</FormLabel>
+              <Input placeholder="Campaign Title" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Position Description</FormLabel>
+              <Textarea placeholder="Enter a position description including key selection criteria, experience, etc" />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Save Campaign
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
