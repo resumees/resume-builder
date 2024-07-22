5ed64ac04c4af32722a32c9378ae25fb761bef00
F-Joubert <fergusjoubert@protonmail.com>
2024-07-22 16:45:28 +1000

    Reformatted file

--- frontend/src/components/Campaign/CreateCampaign.tsx
+++ frontend/src/components/Campaign/CreateCampaign.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { 
-  Box, 
-  Button, 
+import {
+  Box,
+  Button,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -13,44 +13,75 @@ import {
   Textarea,
   FormControl,
   FormLabel,
-  useDisclosure
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
-import { CAMPAIGN_DESCRIPTION_PLACEHOLDER, CAMPAIGN_TITLE_PLACEHOLDER } from "./Campaign.config";
+import {
+  CAMPAIGN_DESCRIPTION_PLACEHOLDER,
+  CAMPAIGN_TITLE_PLACEHOLDER,
+} from "./Campaign.config";
 import React, { useState } from "react";
-import request from "@/util/api";
 import { addCampaign } from "@/reduxFeatures/campaignSlice";
 import { useDispatch } from "react-redux";
-        
+
 const CreateCampaign: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [campaignTitle, setCampaignTitle] = useState("");
   const [campaignDescription, setCampaignDescription] = useState("");
   const dispatch = useDispatch();
+  const toast = useToast();
 
   const handleCampaignTitleChange = (e) => setCampaignTitle(e.target.value);
-  const handleCampaignDescriptionChange = (e) => setCampaignDescription(e.target.value);
+  const handleCampaignDescriptionChange = (e) => {
+    setCampaignDescription(e.target.value);
+  };
 
   const handleSubmit = () => {
-    const newCampaign = {
-      documentName: campaignTitle,
-      documentDescription: campaignDescription,
-    }
-    request(`${import.meta.env.VITE_BACKEND_URL}/api/campaign/new`, "POST", newCampaign)
-      .then(() => {
-        dispatch(addCampaign({
-          _id: "",
-          documentName: campaignTitle,
-          documentDescription: campaignDescription,
-        }));
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/campaign/new`, {
+      method: "POST",
+      body: JSON.stringify({
+        documentName: campaignTitle,
+        documentDescription: campaignDescription,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        dispatch(
+          addCampaign({
+            _id: data.savedCampaignId,
+            documentName: campaignTitle,
+            documentDescription: campaignDescription,
+          })
+        )
       })
       .then(() => {
-        onClose();
-      });
+        toast({
+          title: `Campaign Saved`,
+          description: `${campaignTitle} saved to database.`,
+        })
+      })
+      .then(() => {
+        onClose()
+      })
+      .catch((error) => {
+        console.error("Save campaign error:", error);
+        toast({
+          title: `Save Campaign Error`,
+          description: `Error saving to database: ${error}`,
+        })
+      })
   };
 
   return (
     <Box>
-      <Button colorScheme="blue" onClick={onOpen}>Create Campaign</Button>
+      <Button colorScheme="blue" onClick={onOpen}>
+        Create Campaign
+      </Button>
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -60,18 +91,18 @@ const CreateCampaign: React.FC = () => {
           <ModalBody>
             <FormControl>
               <FormLabel>Campaign Title</FormLabel>
-              <Input 
-              placeholder={CAMPAIGN_TITLE_PLACEHOLDER}
-              value={campaignTitle}
-              onChange={handleCampaignTitleChange} 
+              <Input
+                placeholder={CAMPAIGN_TITLE_PLACEHOLDER}
+                value={campaignTitle}
+                onChange={handleCampaignTitleChange}
               />
             </FormControl>
             <FormControl>
               <FormLabel>Position Description</FormLabel>
-              <Textarea 
-              placeholder={CAMPAIGN_DESCRIPTION_PLACEHOLDER}
-              value={campaignDescription}
-              onChange={handleCampaignDescriptionChange} 
+              <Textarea
+                placeholder={CAMPAIGN_DESCRIPTION_PLACEHOLDER}
+                value={campaignDescription}
+                onChange={handleCampaignDescriptionChange}
               />
             </FormControl>
           </ModalBody>
