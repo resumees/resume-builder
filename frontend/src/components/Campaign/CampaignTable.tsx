8121def3e30f2b500c329818fa4081bfb36bc93d
F-Joubert <fergusjoubert@protonmail.com>
2024-07-22 20:53:18 +1000

    Replaced useEffect with useQuery to refresh table data

--- frontend/src/components/Campaign/CampaignTable.tsx
+++ frontend/src/components/Campaign/CampaignTable.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { ApplicantData } from "@/lib/definitions";
+import request from "@/util/api";
 import {
   Table,
   TableCaption,
@@ -12,6 +13,7 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 
 type CampaignTableProps = {
   campaignId: string;
@@ -22,50 +24,70 @@ export const CampaignTable: React.FC<CampaignTableProps> = ({ campaignId }) => {
   const [campaignData, setCampaignData] = useState([]);
   const toast = useToast();
 
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/campaign/load/${campaignId}`, {
-        credentials: "include"
+  const { isLoading } = useQuery(
+    [`load${campaignId}`, campaignId],
+    () =>
+      fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/api/campaign/load/${campaignId}`,
+        {
+          credentials: "include",
+        }
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setCampaignName(data.currentCampaign.documentName);
+          setCampaignData(data.currentCampaign.applicant);
+        })
+        .catch((error) => {
+          console.error("Load campaign error:", error);
+          toast({
+            title: `Load Campaign Error`,
+            description: `Error loading campaign: ${error}`,
+          });
+        }),
+    {
+      refetchInterval: false,
+      enabled: true,
+      refetchOnMount: "always",
+      refetchOnWindowFocus: false,
+      cacheTime: 600,
+      staleTime: 600,
     }
-    )
-      .then((res) => {
-        return res.json()
-      })
-      .then((data) => {
-        setCampaignName(data.currentCampaign.documentName);
-        setCampaignData(data.currentCampaign.applicant);
-      })
-      .catch((error) => {
-        console.error("Load campaign error:", error);
-        toast({
-          title: `Load Campaign Error`,
-          description: `Error loading campaign: ${error}`,
-        });
-      });
-  });
+  );
 
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <TableCaption>{campaignName} Applicants</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Rank</Th>
-            <Th>Applicant Name</Th>
-            <Th>Date Submitted</Th>
-            <Th>Score</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {campaignData?.map((data: ApplicantData, index: number) => (
-            <Tr key={index + 1}>
-              <Td>{index + 1}</Td>
-              <Td>{data.documentName}</Td>
-              <Td>{data.submittedDate.toString()}</Td>
-              <Td>{data.candidateScore}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <TableContainer>
+            <Table variant="simple">
+              <TableCaption>{campaignName} Applicants</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Rank</Th>
+                  <Th>Applicant Name</Th>
+                  <Th>Date Submitted</Th>
+                  <Th>Score</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {campaignData?.map((data: ApplicantData, index: number) => (
+                  <Tr key={index + 1}>
+                    <Td>{index + 1}</Td>
+                    <Td>{data.documentName}</Td>
+                    <Td>{data.submittedDate.toString()}</Td>
+                    <Td>{data.candidateScore}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+    </>
   );
 };
