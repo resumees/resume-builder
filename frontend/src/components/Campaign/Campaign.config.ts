d08389f1822633f138ff680021d814fc706227e2
F-Joubert <fergusjoubert@protonmail.com>
2024-07-22 20:36:24 +1000

    Updated type definitions for campaign / applicant data

--- frontend/src/components/Campaign/Campaign.config.ts
+++ frontend/src/components/Campaign/Campaign.config.ts
@@ -1,10 +1,9 @@
-import { ApplicantData, ContactDetails, EducationEntry, EmploymentEntry, PersonalProjectEntry } from "@/lib/definitions";
+import { ApplicantData, CampaignData, ContactDetails, EducationEntry, EmploymentEntry, PersonalProjectEntry } from "@/lib/definitions";
 
 export const CAMPAIGN_TITLE_PLACEHOLDER = "Campaign Title";
 export const CAMPAIGN_DESCRIPTION_PLACEHOLDER = "Enter a position description including key selection criteria, experience, etc";
 
 const TEST_CONTACT_DETAILS:ContactDetails = {
-    applicantName: "John Doe",
     applicantEmail: "example@test.com",
     applicantPhone: "040000000",
 };
@@ -25,18 +24,14 @@ const TEST_EMPLOYMENT_LATEST:EmploymentEntry = {
     jobTitle: "Senior Software Engineer",
     companyName: "Test Company",
     jobDescription: "Entire company depended on me",
-    startDate: "2020",
     endDate: "2024",
-    dateRange: null,
 }
 
 const TEST_EMPLOYMENT_PREVIOUS:EmploymentEntry = {
     jobTitle: "Junior Software Engineer",
     companyName: "Test Company",
     jobDescription: "Made a macro to push on save.",
-    startDate: "2018",
     endDate: "2020",
-    dateRange: null,
 }
 
 const TEST_PERSONAL_PROJECT:PersonalProjectEntry = {
@@ -45,12 +40,25 @@ const TEST_PERSONAL_PROJECT:PersonalProjectEntry = {
     projectDate: "2019",
 }
 
+const currentDate = new Date();
+
 export const TEST_APPLICANT_DATA:ApplicantData[] = [{
+    _id: "0",
+    documentName: "John Doe",
+    submittedDate: currentDate,
+    candidateScore: 42,
     contactDetails: TEST_CONTACT_DETAILS,
     educationHistory: [TEST_EDUCATION_MASTERS, TEST_EDUCATION_BACHELORS],
     workExperience: [TEST_EMPLOYMENT_LATEST, TEST_EMPLOYMENT_PREVIOUS],
     personalProjects: [TEST_PERSONAL_PROJECT],
 }]
+
+export const TEST_CAMPAIGN_DATA:CampaignData = {
+    _id: "0",
+    documentName: "Placeholder Campaign",
+    documentDescription: "Placeholder Campaign",
+    applicant: TEST_APPLICANT_DATA,
+}
 
 export type Campaign = {
     _id: string;
