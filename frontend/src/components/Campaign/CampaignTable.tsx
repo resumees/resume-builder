f8b210cee0ab48b4a254807c24a208cafab893a1
F-Joubert <fergusjoubert@protonmail.com>
2024-07-22 20:34:18 +1000

    Update CampaignTable to load from database

--- frontend/src/components/Campaign/CampaignTable.tsx
+++ frontend/src/components/Campaign/CampaignTable.tsx
@@ -1,41 +1,71 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { ApplicantData } from "@/lib/definitions";
-import { Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react"
-import React from "react";
+import {
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
 type CampaignTableProps = {
-    campaignName: string;
-    tableData: ApplicantData[];
-}
+  campaignId: string;
+};
 
-export const CampaignTable: React.FC<CampaignTableProps> = ({ campaignName, tableData }) => {
-    const campaignApplicantData = () => {
-        return tableData.map((data: ApplicantData, index: number) => (
-            <Tr key={index + 1}>
-                <Td>{index + 1}</Td> {/* Needs to be replaced with actual rank from LLM */}
-                <Td>{data.contactDetails.applicantName}</Td> {/* Can either make this a link or add a button to more details */}
-                <Td>13/07/2024</Td>
-                <Td>89%</Td>
-            </Tr>
-        ));
+export const CampaignTable: React.FC<CampaignTableProps> = ({ campaignId }) => {
+  const [campaignName, setCampaignName] = useState("");
+  const [campaignData, setCampaignData] = useState([]);
+  const toast = useToast();
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/campaign/load/${campaignId}`, {
+        credentials: "include"
     }
-
-    return (
-        <TableContainer>
-            <Table variant="simple">
-                <TableCaption>{campaignName} Applicants</TableCaption>
-                <Thead>
-                    <Tr>
-                        <Th>Rank</Th>
-                        <Th>Applicant Name</Th>
-                        <Th>Date Submitted</Th>
-                        <Th>Score</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {campaignApplicantData()}
-                </Tbody>
-            </Table>
-        </TableContainer>
     )
-}
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        setCampaignName(data.currentCampaign.documentName);
+        setCampaignData(data.currentCampaign.applicant);
+      })
+      .catch((error) => {
+        console.error("Load campaign error:", error);
+        toast({
+          title: `Load Campaign Error`,
+          description: `Error loading campaign: ${error}`,
+        });
+      });
+  });
+
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption>{campaignName} Applicants</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Rank</Th>
+            <Th>Applicant Name</Th>
+            <Th>Date Submitted</Th>
+            <Th>Score</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {campaignData?.map((data: ApplicantData, index: number) => (
+            <Tr key={index + 1}>
+              <Td>{index + 1}</Td>
+              <Td>{data.documentName}</Td>
+              <Td>{data.submittedDate.toString()}</Td>
+              <Td>{data.candidateScore}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
